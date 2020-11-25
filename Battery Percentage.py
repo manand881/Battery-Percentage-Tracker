e2db78ff45b1e46d887437992508dfefc31d5259
Anand Mahesh <manand881@gmail.com>
2020-11-25 19:40:18 +0530

    Cleaned up and consolidated

--- Battery Percentage.py
+++ Battery Percentage.py
@@ -1,61 +1,65 @@
 import threading
 import winsound
 import psutil
-import ctypes           #   For Message Box
+import ctypes
 import time
 import sys
 
-def batery_check():
+if psutil.sensors_battery() is None:
+    ctypes.windll.user32.MessageBoxW(0, "This Computer may not have a battery or may be damaged.", "Battery Status", 0)
+    sys.exit()
 
+
+def batery_check():
     battery = psutil.sensors_battery()
     plugged = battery.power_plugged
     percent = battery.percent
-    
-    if plugged==False: 
-        plugged="Not Plugged In"
-    else: 
-        plugged="Plugged In"
-    
-    return percent,plugged
+
+    if not plugged:
+        plugged = "Not Plugged In"
+    else:
+        plugged = "Plugged In"
+
+    return percent, plugged
+
 
 def beep():
-
-    frequency = 2500    #   Set Frequency To 2500 Hertz
-    duration = 2000     #   Set Duration To 1000 ms == 1 second
+    frequency = 2500  # Set Frequency To 2500 Hertz
+    duration = 2000  # Set Duration To 1000 ms == 1 second
     winsound.Beep(frequency, duration)
-  
+
+
 def messagebox():
-    
-    ctypes.windll.user32.MessageBoxW(0, "Unplug Charger now to avoid battery damage!", "Battery Status",0)
-    
+    ctypes.windll.user32.MessageBoxW(0, "Unplug Charger now to avoid battery damage!", "Battery Status", 0)
+
+
 def check():
+    flag = True
+    messageflag = False
 
-    flag=True
-    messageflag=False
+    while flag:
 
-    while flag==True:
-
-        percent,plugged=batery_check()
+        percent, plugged = batery_check()
         sys.stdout.flush()
-        sys.stdout.write('\rRunning program, battery at '+str(percent)+"% " + plugged)
+        sys.stdout.write('\rRunning program, battery at ' + str(percent) + "% " + plugged)
         sys.stdout.flush()
-        
-        if plugged =="Plugged In":
-            if percent>=95:
-                
+
+        if plugged == "Plugged In":
+            if percent >= 95:
+
                 beep()
 
-                if messageflag==False:
-                    message=threading.Thread(target=messagebox)
+                if not messageflag:
+                    message = threading.Thread(target=messagebox)
                     message.start()
-                    messageflag=True
+                    messageflag = True
 
         else:
-            if percent<=95:
-                    messageflag=False
-
+            if percent <= 95:
+                messageflag = False
 
         time.sleep(2)
+
 
 t = threading.Thread(target=check)
 t.start()
--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# Battery_percentage
-A python app that provides alerts when your battery is completely charged. converted to a microsoft app through auto-py-to-exe
+# Battery Percentage Tracker
+
+This repository contains code for a simple project which tracks your battery percentage on your laptop and alerts you to unplug your charger to avoid damage.
+
+
+## Installation
+
+Use the package manager [pip](https://pip.pypa.io/en/stable/) to install the following requirements essential to running the project.
+
+```bash
+pip install psutil
+```
+## Usage
+
+You can choose to copy and paste the `.exe` file which has the same name as the program in your startup folder in your system to get the program running from system start incase you dont want to be bothered to run the program manually all the time
+
+
+## Contributing
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+Please make sure to update tests as appropriate.
+
+## License
+[The Unlicense ](https://choosealicense.com/licenses/unlicense/)
